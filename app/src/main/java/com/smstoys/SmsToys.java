271a7df7d8fd7bdb367645cf9420902b9863249c
Ether009 <ether009@gmail.com>
2016-12-12 14:54:16 +0100

    Initial commit

---
+++ app/src/main/java/com/smstoys/SmsToys.java
@@ -0,0 +1,13 @@
+package com.smstoys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SmsToys extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sms_toys);
+    }
+}
